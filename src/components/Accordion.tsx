376e7df9740dd22c27cc7b15e2310fc8cc0ba87e
Prodyut <prodyut8765@gmail.com>
2023-01-23 23:10:53 +0530

    update: checking contri

--- src/components/Accordion.tsx
+++ src/components/Accordion.tsx
@@ -13,16 +13,16 @@ export default function Accordion(props: any) {
                             <input type="checkbox" id={`chck${index + 1}`} />
                             <label className="tab-label border-bot" htmlFor={`chck${index + 1}`} ><span><img src={index === 0 ? capgemini : cisco} height='30' /><p>{item.duration}</p></span></label>
                             <div className="tab-content">
-                                {item.projects.map(project => 
+                                {item.projects.map(project =>
                                     <>
-                                    <label><b>Title: </b>{project.title}</label>
-                                    <div><b>Description: </b>{project.desc}</div>
-                                    <div><b>Role:</b> {project.role}</div>
-                                    <ul>
-                                        {project.work.map(task=>
-                                           <li>{task}</li>
-                                        )}
-                                    </ul>
+                                        <label><b>Title: </b>{project.title}</label>
+                                        <div><b>Description: </b>{project.desc}</div>
+                                        <div><b>Role:</b> {project.role}</div>
+                                        <ul>
+                                            {project.work.map(task =>
+                                                <li>{task}</li>
+                                            )}
+                                        </ul>
                                     </>
                                 )}
                             </div>
